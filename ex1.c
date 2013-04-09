73ad8541834b12ddfdee1e73d6dbe02e625baf82
Drachis <drachis@gmail.com>
2013-04-09 06:21:05 -0700

    adding  learning c files to git hub to keep track of my progress and get used to using git regularly

---
+++ ex1.c
@@ -0,0 +1,29 @@
+#include "ex1.h"
+
+int main(int argc,char *argv[])
+{
+    puts("Hello world.\n");
+    ex3();
+  	ex4();
+    return 0;
+}
+
+void ex3()
+{
+  int age = 10;
+  int height = 72;
+  
+  printf("I am %d years old.\n", age);
+  printf("I am %d inches tall.\n",height);
+
+}
+
+void ex4()
+{
+  int age = 26;
+  int height=76; 
+  
+  printf("I am %d years old.\n",age);
+  printf("I am %d inches tall.\n",height);
+  
+}
---
+++ ex1.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+//simple header file, will need to be updated when new functions are added.
+void ex3(),ex4();
+
