70c73377aa4daa180ff75415d71c42981a10ca47
Drachis <drachis@gmail.com>
2013-05-04 01:21:28 -0700

    removed all functions from ex1 to 15 from ex1 got tired to scrolling and realized I had version control so I don't need to school down dead code.

--- ex1.h
+++ ex1.h
@@ -1,13 +1,1 @@
 #include <stdio.h>
-#include <ctype.h>
-
-//simple header file, will need to be updated when new functions are added.
-
-void ex3(),ex4(),ex6(),
-	ex7(),ex8(),ex9(),
-	ex10(),ex11(),ex12(),
-	ex13(),ex14();
-//for ex14
-int can_print_it(char ch);
-void print_letters(char arg[]);
-void print_arguments(int argc, char *argv[]);
