3ce5214433de7b744f246c74381b1b2df880d465
Toli Carter <drachis@gmailcom>
2013-05-11 15:53:42 -0700

    added the headers for ex 17 to c.h, trying more foward declerations than usual since there are more functions, this may require additional debugging.

--- ex1.h
+++ ex1.h
@@ -7,9 +7,38 @@
 #define MAX_DAT 512
 #define MAX_ROWS 100
 
-struct Address {
-  int id;
-  int set;
-  char name[MAX_DATA];
-  char email[MAX_DATA];
-}
+struct Address(){};
+struct Database(){};
+struct Connnection(){};
+struct Connection *Database_open(
+	const char *filename
+	,char mode
+	){};
+void die(
+	const char *message
+	){};
+void Adress_print(
+	struct Adress *addr
+	){};
+void Database_close(
+	struct Connection *conn
+	){};
+void Database_create(
+	struct Connection *conn
+	){};
+void Database_set(
+	struct Connection *conn
+	,int id
+	,const char *name
+	,const char *email
+	){};
+void Database_get(
+	struct Connection *conn
+	, int id){};
+void Database_delete(
+	struct Connection *conn
+	, int id
+	){};
+void Database_list(
+	struct Connection 8conn
+	){};
