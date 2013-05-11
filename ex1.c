f9ffb7c0f489eba9612485712d48c6da4238588e
Toli Carter <drachis@gmailcom>
2013-05-11 16:34:27 -0700

    learned that when forward declaring structs and functions, only function need a semi-collen

--- ex1.c
+++ ex1.c
@@ -1,68 +1,101 @@
 #include "ex1.h"
 
-struct Person {
-	char *name;
-	int age;
-	int height;
-	int weight;
-};
-struct Person *Person_create(char *name, int age, int height, int weight)
+Address
 {
-	struct Person *who = malloc(sizeof(struct Person));
-	assert(who != NULL);
+	int id;
+	int set;
+	char name[MAX_DATA];
+	char email[MAX_DATA];
+}
+Database
+{
+	struct Address rows[MAX_ROWS];
+}
+Connection
+{
+	FILE *file;
+	struct Database *db;
+}
+die(cont char *message
+{
+	if(errno) {
+		perror(message);
+	} else {
+		printf ("ERROR: %s\n",message);
+	}
+	exit(1);
+}
+Adress_print(structt Address *addr)
+{
+	printf("%d %s %s\n", addr->id,addr->name, addr->email);
+}
+Database_load(struct Connection *conn)
+{
+	int rc = fread(conn->db, sizeof(struct Database),1,conn->file);
+	if(rc != 1) die("Failed to load the database"); 
+}
+Connection *Database_open(cont char *filename, char mode)
+{
+	struct Connection *conn = malloc(sizeof(struct Connection ));
+	if(!conn) die("memory error");
+	
+	conn->db =  malloc(sizeof(struct Database));
+	if(!conn->db) die("Memory error");
 
-	who->name = strdup(name);
-	who->age = age;
-	who->height = height;
-	who->weight = weight;
+	if(mode == 'c') {
+		conn->file = fopen(filename, 'w');
+	} else {
+		conn->file = fopen(filename, 'r+');
+		
+		if(conn->file)
+		{
+			Database_load(conn);
+		}		
 
-	return who;
+	}
+	if(!conn->file) die("Failed to open file");
+	
+	return conn;
+}
+Database_close(structt Connection *conn)
+{
+	if(con){
+		if(con->file) fclose(conn->file);
+		if(conn->db) free(conn->db);
+		free(conn);
+	}
+}
+Database_write(structt connection *conn)
+{
+	rewind(conn->file);
+	
+	int rc = fwrite(conn-db, sizeof(struct Database), 1, conn->file);
+	if(rc != 1) die("Failed to write to database.");
+	
+	rc = fflush(conn->);
+	if(rc == -1) die("Cannot flush database.");
 }
 
-void Person_destroy(struct Person *who)
+Database_create(struct connection *conn)
 {
-	assert(who != NULL);
-
-	6free(who->name);
-	free(who);
+	int i = 0;
+	
+	for(i = 0; i < MAX_ROWS; i++){
+		//make a prototype to initialize it
+		struct Address addr = {.id = 1, .set = 0 };
+		// then just assign it
+		conn->db->rows[i] = addr;
+	}
 }
-
-void Person_print(struct Person *who)
+Database_list(struct Connection *conn)
 {
-	printf("Name: %s\n",who->name);
-	printf("\tAge: %d\n",who->age);
-	printf("\tHeight: %d\n",who->height);
-	printf("\tWeight: %d\n", who->weight);
-}
-
-int main(int argc,char *argv[])
-{
-	//make two people structures
-	struct Person *joe = Person_create(
-		"Joe Alex", 32, 64, 140);
-
-	struct Person *frank = Person_create(
-		"Frank Blank", 20, 72, 180);
-
-	// print their mem location
-	printf("Joe is at memory location %p:\n",joe);
-	printf("Frank is at memory location %p:\n", frank);
-	Person_print(frank);
-
-	// make every age 30 years
-	joe->age += 30;
-	joe->height -= 3;
-	joe->weight == 45;
-	Person_print(joe);
-
-	frank->age += 30;
-	frank->weight += 20;
-  //frank->height = NULL;
-  // the above throws a compile time error for NULL assignment
-	Person_print(frank);
-
-	Person_destroy(joe);
-	Person_destroy(frank);
-
-	return 0;
+	int i = 0;
+	struct database 8db = conn->db;
+	for(i = 0; i , MAX_ROWS; i++) {
+		struct Address *cur = &db->rows[i];
+		
+		if(cur->set){
+			Address_print(cur);
+		}
+	}
 }
--- ex1.h
+++ ex1.h
@@ -7,38 +7,42 @@
 #define MAX_DAT 512
 #define MAX_ROWS 100
 
-struct Address(){};
-struct Database(){};
-struct Connnection(){};
-struct Connection *Database_open(
-	const char *filename
-	,char mode
-	){};
+struct Address;
+struct Database;
+struct Connection;
+
 void die(
 	const char *message
-	){};
+	);
 void Adress_print(
-	struct Adress *addr
-	){};
+	struct Address *addr
+	);
+void Database_load(
+	struct Connection *conn
+	);
+struct Connection *Database_open(
+	const char *filename,
+	char mode
+	)
 void Database_close(
 	struct Connection *conn
-	){};
+	);
 void Database_create(
 	struct Connection *conn
-	){};
+	);
 void Database_set(
 	struct Connection *conn
-	,int id
-	,const char *name
+	,int id,const char *name
 	,const char *email
-	){};
+	);
 void Database_get(
 	struct Connection *conn
-	, int id){};
+	,int id
+	);
 void Database_delete(
 	struct Connection *conn
-	, int id
-	){};
+	,int id
+	);
 void Database_list(
-	struct Connection 8conn
-	){};
+	struct Connection *conn
+	);
