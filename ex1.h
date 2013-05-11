6a82d909f11484ada605c944e45d82db25449b73
Toli Carter <drachis@gmailcom>
2013-05-11 13:54:42 -0700

    ex17 h file

--- ex1.h
+++ ex1.h
@@ -1,5 +1,15 @@
 #include <stdio.h>
 #include <assert.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_DAT 512
+#define MAX_ROWS 100
+
+struct Address {
+  int id;
+  int set;
+  char name[MAX_DATA];
+  char email[MAX_DATA];
+}
