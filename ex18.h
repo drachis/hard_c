49b3ea40f6228a6a955b0a38eaae919084acc561
Drachis <drachis@gmail.com>
2013-05-15 12:22:20 -0700

    ex18 typed out and up and running

--- ex18.h
+++ ex18.h
@@ -1,1 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+void die( const char *message);
+// a typedef creates a fale type, 
+// in this case a function pointer
+typedef int (*compare_cb)(int a, int b);
+
+int *bubble_sort(int *numbers, int count, compare_cb cmp);
+
+int sorted_order(int a, int b);
+int reverse_order(int a, int b);
+int strange_order(int a, int b);
+
+void test_sorting(int *numbers, int count, compare_cb cmp);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
