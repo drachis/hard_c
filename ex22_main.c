fde97cd606dd5533fdf25afc1c3a72d874c63391
Drachis <drachis@gmail.com>
2013-05-28 23:25:13 -0700

    cleaning up errors

--- ex22_main.c
+++ ex22_main.c
@@ -40,15 +40,15 @@ int main(int argc, char *argv[])
 	
 	// test the ratio function static
 	log_info("Ratio at first: %f", update_ratio(2.0));
-	log_info("Ratio again: %f", update(10.0));
-	log_info("Ratio once more: %f", nupdate_ratio(300.0));
+	log_info("Ratio again: %f", update_ratio(10.0));
+	log_info("Ratio once more: %f", update_ratio(300.0));
 	
 	// test the scope demo
 	int count = 4;
 	scope_demo(count);
 	scope_demo(count * 20);
 	
-	log_info("count after calling scope_demo: %d", count);
+	log_info("count after calling scope_demo: %s", count);
 	
 	return 0;
 }
